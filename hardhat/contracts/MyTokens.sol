0e693879360a217da5155a619c8f1d4c62fd5d1a
Ashik Rahman <ashrhmn99@gmail.com>
2022-01-24 22:06:05 +0600

    Add buy and swap, and test functions

--- hardhat/contracts/MyTokens.sol
+++ hardhat/contracts/MyTokens.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "hardhat/console.sol";
 
-contract MyTokens is ERC1155 {
+contract MyTokens is ERC1155, ERC1155Holder {
     using SafeMath for uint256;
     struct Token {
         string name;
@@ -16,10 +17,13 @@ contract MyTokens is ERC1155 {
 
     uint256[] public tokenEquivalenceOfSingleEther;
 
+    uint256 public floatingPoint;
+
     constructor(
         string[] memory _tokenNames,
         uint256[] memory _tokenSupplies,
-        uint256[] memory _tokenPriceEquivalence
+        uint256[] memory _tokenPriceEquivalence,
+        uint256 _floatingPoint
     ) ERC1155("https://game.example/api/item/{id}.json") {
         require(
             _tokenNames.length == _tokenSupplies.length &&
@@ -27,11 +31,29 @@ contract MyTokens is ERC1155 {
             "You must provide equal numbers of name, tokenSupplies and equivalentPrice"
         );
 
+        // require(
+        //     _floatingPoint >= 10,
+        //     "Floating point should be greater/equal than/to 10"
+        // );
+
+        floatingPoint = _floatingPoint;
+
         for (uint256 i = 0; i < _tokenNames.length; ++i) {
             tokens.push(Token(_tokenNames[i], _tokenSupplies[i]));
-            _mint(msg.sender, i, _tokenSupplies[i], "");
+            _mint(address(this), i, _tokenSupplies[i] * 10**floatingPoint, "");
             tokenEquivalenceOfSingleEther.push(_tokenPriceEquivalence[i]);
         }
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155, ERC1155Receiver)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IERC1155Receiver).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 
     function tokenToEquivalentWei(uint256 tokenId, uint256 amount)
@@ -42,10 +64,55 @@ contract MyTokens is ERC1155 {
         amount = amount * 10**18;
         uint256 singleEtherAmountToken = tokenEquivalenceOfSingleEther[tokenId];
         uint256 result = amount.div(singleEtherAmountToken);
-        console.log(amount);
-        console.log(singleEtherAmountToken);
-        console.log(result);
+        // console.log(amount);
+        // console.log(singleEtherAmountToken);
+        // console.log(result);
         return result;
+    }
+
+    function tokenAequivalentTokenB(
+        uint256 tokenIdA,
+        uint256 tokenIdB,
+        uint256 amount
+    ) public view returns (uint256) {
+        uint256 tokenValueA = tokenEquivalenceOfSingleEther[tokenIdA];
+        uint256 tokenValueB = tokenEquivalenceOfSingleEther[tokenIdB];
+        amount = amount * tokenValueB;
+        uint256 equivalentvalue = amount.div(tokenValueA);
+        return equivalentvalue;
+    }
+
+    function buyToken(uint256 tokenId, uint256 amount) public payable {
+        console.log("buy token : msg.value");
+        console.log(msg.value);
+        console.log(tokenToEquivalentWei(tokenId, amount));
+
+        require(
+            msg.value >= tokenToEquivalentWei(tokenId, amount) - 1000, //leaving out 1000wei for floating value error
+            "Amount not provided, buy token"
+        );
+        IERC1155(address(this)).safeTransferFrom(
+            address(this),
+            msg.sender,
+            tokenId,
+            amount,
+            ""
+        );
+    }
+
+    function swapToken(
+        uint256 tokenIdA,
+        uint256 tokenIdB,
+        uint256 amount
+    ) public payable {
+        safeTransferFrom(msg.sender, address(this), tokenIdA, amount, "");
+        IERC1155(address(this)).safeTransferFrom(
+            address(this),
+            msg.sender,
+            tokenIdB,
+            tokenAequivalentTokenB(tokenIdA, tokenIdB, amount),
+            ""
+        );
     }
 
     function getTokens() public view returns (Token[] memory) {
--- hardhat/test/sample-test.js
+++ hardhat/test/sample-test.js
@@ -31,7 +31,7 @@ describe("MyTokens", async function() {
         deployer = accounts[0]
         const MyTokens = await ethers.getContractFactory("MyTokens")
         myTokens = await MyTokens.deploy(
-            tokenNames, tokenSupplies, tokenEquivalencePrice
+            tokenNames, tokenSupplies, tokenEquivalencePrice, 0
         )
         await myTokens.deployed()
 
@@ -47,16 +47,16 @@ describe("MyTokens", async function() {
 
         // console.log("GOLD Balance of owner : ", await balanceOf(deployer.address, 0));
 
-        expect(await balanceOfFn(deployer.address, 0)).to.equal("3000000000000000")
-        expect(await balanceOfFn(deployer.address, 1)).to.equal("1000000000000000")
-        expect(await balanceOfFn(deployer.address, 2)).to.equal("4000000000000000")
+        expect(await balanceOfFn(myTokens.address, 0)).to.equal("3000000000000000")
+        expect(await balanceOfFn(myTokens.address, 1)).to.equal("1000000000000000")
+        expect(await balanceOfFn(myTokens.address, 2)).to.equal("4000000000000000")
     });
-    it("should have accurate balance after transfer", async() => {
-        const safeTransferFromTx = await myTokens.safeTransferFrom(deployer.address, accounts[2].address, 0, 1000, [])
-        await safeTransferFromTx.wait()
-        expect(await balanceOfFn(accounts[2].address, 0)).to.equal("1000")
-        expect(await balanceOfFn(deployer.address, 0)).to.equal("2999999999999000")
-    })
+    // it("should have accurate balance after transfer", async() => {
+    //     const safeTransferFromTx = await myTokens.safeTransferFrom(myTokens.address, accounts[2].address, 0, 1000, [])
+    //     await safeTransferFromTx.wait()
+    //     expect(await balanceOfFn(accounts[2].address, 0)).to.equal("1000")
+    //     expect(await balanceOfFn(myTokens.address, 0)).to.equal("2999999999999000")
+    // })
     it("Should calculate equivalent prices correctly", async() => {
         const amount = 1000
         for (const [index, price] of tokenEquivalencePrice.entries()) {
@@ -64,5 +64,213 @@ describe("MyTokens", async function() {
                 // console.log(weiToEth(requiredWeiForToken, 8))
             expect(weiToEth(tokenToEquivalentWei, 8)).to.be.approximately(amount / price, 1e-4)
         }
+    })
+
+    it("Should calculate tokenAequivalentTokenB value 1", async() => {
+        const amount = 1000;
+        const indexA = 0;
+        const indexB = 1;
+        const tokenToEquivalentWei = (await myTokens.tokenAequivalentTokenB(indexA, indexB, amount)).toString()
+        expect(tokenToEquivalentWei).be.equal((tokenEquivalencePrice[indexB] * amount / tokenEquivalencePrice[indexA]).toFixed(0))
+    })
+
+    it("Should calculate tokenAequivalentTokenB value 2", async() => {
+        const amount = 1000;
+        const indexA = 1;
+        const indexB = 2;
+        const tokenToEquivalentWei = (await myTokens.tokenAequivalentTokenB(indexA, indexB, amount)).toString()
+        expect(tokenToEquivalentWei).be.equal((tokenEquivalencePrice[indexB] * amount / tokenEquivalencePrice[indexA]).toString().split('.')[0])
+    })
+
+    it("Should buy token and balance get updated", async() => {
+
+        const stage1EthBalance = await myTokens.provider.getBalance(accounts[0].address)
+        const stage1Token0Balance = await balanceOfFn(accounts[0].address, 0)
+
+        // console.log("Eth balance s1", stage1EthBalance)
+        // console.log("Token0 balance s1", stage1Token0Balance)
+
+        const buyTokenTx = await myTokens.buyToken(0, 1000, { value: (1000 * 1e18 / tokenEquivalencePrice[0]).toString() })
+        await buyTokenTx.wait()
+
+        const stage2EthBalance = await myTokens.provider.getBalance(accounts[0].address)
+        const stage2Token0Balance = await balanceOfFn(accounts[0].address, 0)
+
+        // console.log("Eth balance s2", stage2EthBalance)
+        // console.log("Token0 balance s2", stage2Token0Balance)
+
+        expect(parseInt(stage1EthBalance)).be.greaterThan(parseInt(stage2EthBalance))
+        expect(parseInt(stage1Token0Balance)).be.lessThan(parseInt(stage2Token0Balance))
+
+        const buyTokenTx2 = await myTokens.buyToken(0, 1000, { value: (1000 * 1e18 / tokenEquivalencePrice[0]).toString() })
+        await buyTokenTx2.wait()
+
+        const stage3EthBalance = await myTokens.provider.getBalance(accounts[0].address)
+        const stage3Token0Balance = await balanceOfFn(accounts[0].address, 0)
+
+        // console.log("Eth balance s3", stage3EthBalance)
+        // console.log("Token0 balance s3", stage3Token0Balance)
+
+        expect(parseInt(stage2EthBalance)).be.greaterThan(parseInt(stage3EthBalance))
+        expect(parseInt(stage2Token0Balance)).be.lessThan(parseInt(stage3Token0Balance))
+
+    })
+
+    it("Should buy token for different account", async() => {
+
+        const stage1EthBalance = await myTokens.provider.getBalance(accounts[3].address)
+        const stage1Token0Balance = await balanceOfFn(accounts[3].address, 0)
+
+        // console.log("Eth balance s1", stage1EthBalance)
+        // console.log("Token0 balance s1", stage1Token0Balance)
+
+        const buyTokenTx = await myTokens.connect(accounts[3]).buyToken(0, 1000, { value: (1000 * 1e18 / tokenEquivalencePrice[0]).toString() })
+        await buyTokenTx.wait()
+
+        const stage2EthBalance = await myTokens.provider.getBalance(accounts[3].address)
+        const stage2Token0Balance = await balanceOfFn(accounts[3].address, 0)
+
+        // console.log("Eth balance s2", stage2EthBalance)
+        // console.log("Token0 balance s2", stage2Token0Balance)
+
+        expect(parseInt(stage1EthBalance)).be.greaterThan(parseInt(stage2EthBalance))
+        expect(parseInt(stage1Token0Balance)).be.lessThan(parseInt(stage2Token0Balance))
+
+        const buyTokenTx2 = await myTokens.connect(accounts[3]).buyToken(0, 1000, { value: (1000 * 1e18 / tokenEquivalencePrice[0]).toString() })
+        await buyTokenTx2.wait()
+
+        const stage3EthBalance = await myTokens.provider.getBalance(accounts[3].address)
+        const stage3Token0Balance = await balanceOfFn(accounts[3].address, 0)
+
+        // console.log("Eth balance s3", stage3EthBalance)
+        // console.log("Token0 balance s3", stage3Token0Balance)
+
+        expect(parseInt(stage2EthBalance)).be.greaterThan(parseInt(stage3EthBalance))
+        expect(parseInt(stage2Token0Balance)).be.lessThan(parseInt(stage3Token0Balance))
+
+    })
+
+    it("Should buy token for different account index5", async() => {
+
+        const stage1EthBalance = await myTokens.provider.getBalance(accounts[5].address)
+        const stage1Token0Balance = await balanceOfFn(accounts[5].address, 2)
+
+        // console.log("Eth balance s1", stage1EthBalance)
+        // console.log("Token0 balance s1", stage1Token0Balance)
+
+        const buyTokenTx = await myTokens.connect(accounts[5]).buyToken(2, 1000, { value: (1000 * 1e18 / tokenEquivalencePrice[2]).toString() })
+        await buyTokenTx.wait()
+
+        const stage2EthBalance = await myTokens.provider.getBalance(accounts[5].address)
+        const stage2Token0Balance = await balanceOfFn(accounts[5].address, 2)
+
+        // console.log("Eth balance s2", stage2EthBalance)
+        // console.log("Token0 balance s2", stage2Token0Balance)
+
+        expect(parseInt(stage1EthBalance)).be.greaterThan(parseInt(stage2EthBalance))
+        expect(parseInt(stage1Token0Balance)).be.lessThan(parseInt(stage2Token0Balance))
+
+        const buyTokenTx2 = await myTokens.connect(accounts[5]).buyToken(2, 1000, { value: (1000 * 1e18 / tokenEquivalencePrice[2]).toString() })
+        await buyTokenTx2.wait()
+
+        const stage3EthBalance = await myTokens.provider.getBalance(accounts[5].address)
+        const stage3Token0Balance = await balanceOfFn(accounts[5].address, 2)
+
+        // console.log("Eth balance s3", stage3EthBalance)
+        // console.log("Token0 balance s3", stage3Token0Balance)
+
+        expect(parseInt(stage2EthBalance)).be.greaterThan(parseInt(stage3EthBalance))
+        expect(parseInt(stage2Token0Balance)).be.lessThan(parseInt(stage3Token0Balance))
+
+    })
+
+    it("Should test swap func", async() => {
+
+        const userIndex = 3;
+        const tokenIdA = 0;
+        const tokenIdB = 1;
+        const amount = 573;
+        const equivalentTokenB = (amount * tokenEquivalencePrice[tokenIdB] / tokenEquivalencePrice[tokenIdA]).toString().split('.')[0]
+
+        const beforeAccountBalanceA = await balanceOfFn(accounts[userIndex].address, tokenIdA)
+        const beforeTokenBalanceA = await balanceOfFn(myTokens.address, tokenIdA)
+
+        const beforeAccountBalanceB = await balanceOfFn(accounts[userIndex].address, tokenIdB)
+        const beforeTokenBalanceB = await balanceOfFn(myTokens.address, tokenIdB)
+
+        // console.log("Contract A", beforeTokenBalanceA)
+        // console.log("User A", beforeAccountBalanceA)
+
+        // console.log("Contract B", beforeTokenBalanceB)
+        // console.log("User B", beforeAccountBalanceB)
+
+        const swapTokenTx = await myTokens.connect(accounts[userIndex]).swapToken(tokenIdA, tokenIdB, amount)
+        await swapTokenTx.wait()
+
+        // console.log("\n\nTransacted...\n")
+        // console.log(`User swaped ${amount} Token A out and took ${equivalentTokenB} Token B\n\n`)
+
+        const afterAccountBalanceA = await balanceOfFn(accounts[userIndex].address, tokenIdA)
+        const afterTokenBalanceA = await balanceOfFn(myTokens.address, tokenIdA)
+
+        const afterAccountBalanceB = await balanceOfFn(accounts[userIndex].address, tokenIdB)
+        const afterTokenBalanceB = await balanceOfFn(myTokens.address, tokenIdB)
+
+        // console.log("User A", afterAccountBalanceA)
+        // console.log("Contract A", afterTokenBalanceA)
+
+        // console.log("User B", afterAccountBalanceB)
+        // console.log("Contract B", afterTokenBalanceB)
+
+        expect(parseInt(afterAccountBalanceA)).be.equal(parseInt(beforeAccountBalanceA) - amount)
+        expect(parseInt(afterTokenBalanceA)).be.equal(parseInt(beforeTokenBalanceA) + amount)
+        expect(parseInt(afterAccountBalanceB)).be.equal(parseInt(beforeAccountBalanceB) + parseInt(equivalentTokenB))
+        expect(parseInt(afterTokenBalanceB)).be.equal(parseInt(beforeTokenBalanceB) - parseInt(equivalentTokenB))
+
+    })
+
+    it("Should test swap func again index5", async() => {
+
+        const userIndex = 5;
+        const tokenIdA = 2;
+        const tokenIdB = 1;
+        const amount = 329;
+        const equivalentTokenB = (amount * tokenEquivalencePrice[tokenIdB] / tokenEquivalencePrice[tokenIdA]).toString().split('.')[0]
+
+        const beforeAccountBalanceA = await balanceOfFn(accounts[userIndex].address, tokenIdA)
+        const beforeTokenBalanceA = await balanceOfFn(myTokens.address, tokenIdA)
+
+        const beforeAccountBalanceB = await balanceOfFn(accounts[userIndex].address, tokenIdB)
+        const beforeTokenBalanceB = await balanceOfFn(myTokens.address, tokenIdB)
+
+        // console.log("Contract A", beforeTokenBalanceA)
+        // console.log("User A", beforeAccountBalanceA)
+
+        // console.log("Contract B", beforeTokenBalanceB)
+        // console.log("User B", beforeAccountBalanceB)
+
+        const swapTokenTx = await myTokens.connect(accounts[userIndex]).swapToken(tokenIdA, tokenIdB, amount)
+        await swapTokenTx.wait()
+
+        // console.log("\n\nTransacted...\n")
+        // console.log(`User swaped ${amount} Token A out and took ${equivalentTokenB} Token B\n\n`)
+
+        const afterAccountBalanceA = await balanceOfFn(accounts[userIndex].address, tokenIdA)
+        const afterTokenBalanceA = await balanceOfFn(myTokens.address, tokenIdA)
+
+        const afterAccountBalanceB = await balanceOfFn(accounts[userIndex].address, tokenIdB)
+        const afterTokenBalanceB = await balanceOfFn(myTokens.address, tokenIdB)
+
+        // console.log("User A", afterAccountBalanceA)
+        // console.log("Contract A", afterTokenBalanceA)
+
+        // console.log("User B", afterAccountBalanceB)
+        // console.log("Contract B", afterTokenBalanceB)
+
+        expect(parseInt(afterAccountBalanceA)).be.equal(parseInt(beforeAccountBalanceA) - amount)
+        expect(parseInt(afterTokenBalanceA)).be.equal(parseInt(beforeTokenBalanceA) + amount)
+        expect(parseInt(afterAccountBalanceB)).be.equal(parseInt(beforeAccountBalanceB) + parseInt(equivalentTokenB))
+        expect(parseInt(afterTokenBalanceB)).be.equal(parseInt(beforeTokenBalanceB) - parseInt(equivalentTokenB))
+
     })
 });
